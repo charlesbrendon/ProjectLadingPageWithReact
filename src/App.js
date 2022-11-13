fe8458bb4790ea3963b2f5a7158cd799c9095611
Charles Brendon <charlesbrendonss@gmail.com>
2022-11-13 18:12:56 -0300

    Create components lading page pt2

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
 import "./App.css";
+import Conteudo from "./assets/components/Conteudo";
+import Main from "./assets/components/Main";
 
 function App() {
   return (
     <div className="App">
-      <div className="main">
-        <div className="center">
-          <div className="menu">
-            <div className="logo">
-              <h3>Charles.Code</h3>
-            </div>
-            <div className="item-menu">
-              <a href="#">Login</a>
-            </div>
-          </div>
-
-          <div className="form">
-            <h2>Entre em Contato!</h2>
-            <form>
-              <div className="items-form">
-                <input placeholder="Seu nome..." type="text" />
-                <input placeholder="Seu nome..." type="text" />
-                <input placeholder="Seu nome..." type="text" />
-                <input placeholder="Seu nome..." type="text" />
-                <input type="submit" />
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className="conteudo">
-        <div className="center">
-          <div className="conteudo-single">
-            <h3>Titulo da chamada</h3>
-            <p>
-              {" "}
-              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
-              youtube! Estou gostando de consumir ese material
-            </p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Titulo da chamada</h3>
-            <p>
-              {" "}
-              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
-              youtube! Estou gostando de consumir ese material
-            </p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Titulo da chamada</h3>
-            <p>
-              {" "}
-              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
-              youtube! Estou gostando de consumir ese material
-            </p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Titulo da chamada</h3>
-            <p>
-              {" "}
-              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
-              youtube! Estou gostando de consumir ese material
-            </p>
-          </div>
-        </div>
-      </div>
+      <Main />
+      <Conteudo />
     </div>
   );
 }
---
+++ src/assets/components/Conteudo/index.js
@@ -0,0 +1,42 @@
+function Conteudo(){
+    return (
+        <div className="conteudo">
+        <div className="center">
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Conteudo
