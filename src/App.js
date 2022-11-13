201508f3ddbd79da6886cd391831e933a14e0c25
Charles Brendon <charlesbrendonss@gmail.com>
2022-11-13 18:00:23 -0300

    Projeto landing page pt1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+              <h3>Charles.Code</h3>
+            </div>
+            <div className="item-menu">
+              <a href="#">Login</a>
+            </div>
+          </div>
+
+          <div className="form">
+            <h2>Entre em Contato!</h2>
+            <form>
+              <div className="items-form">
+                <input placeholder="Seu nome..." type="text" />
+                <input placeholder="Seu nome..." type="text" />
+                <input placeholder="Seu nome..." type="text" />
+                <input placeholder="Seu nome..." type="text" />
+                <input type="submit" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="conteudo">
+        <div className="center">
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+          <div className="conteudo-single">
+            <h3>Titulo da chamada</h3>
+            <p>
+              {" "}
+              Aqui é o meu conteúdo de exemplo para aprendizado atraves do
+              youtube! Estou gostando de consumir ese material
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
